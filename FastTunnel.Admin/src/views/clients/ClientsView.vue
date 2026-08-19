098d53c50a2e59866585aa64eded9c14239389fb
Johana Ĉen <9k7b5vv9yh@privaterelay.appleid.com>
2026-08-19 19:51:36 +0700

    style(admin): 在线客户端表格令牌列前置并更名"令牌"，IP 列后移

--- FastTunnel.Admin/src/views/clients/ClientsView.vue
+++ FastTunnel.Admin/src/views/clients/ClientsView.vue
@@ -20,8 +20,8 @@ const filteredData = computed(() => {
 })
 
 const columns = computed(() => [
+  { title: '令牌', dataIndex: 'tokenPreview', width: 200, ellipsis: true },
   { title: 'IP', dataIndex: 'ip', width: 140 },
-  { title: 'Token', dataIndex: 'tokenPreview', width: 200, ellipsis: true },
   { title: t('table.column.onlineStatus'), slotName: 'online', width: 100 },
   { title: t('table.column.lastSeen'), slotName: 'lastSeen', width: 160 },
 ])
